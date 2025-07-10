d6614cb1662999b4662ae537c35c52e2110c81e8
Ayodeji Bankole <bankymono@gmail.com>
2025-07-10 21:07:43 +0100

    #

--- src/main/java/org/example/sec01/publisher/SubscriptionImpl.java
+++ src/main/java/org/example/sec01/publisher/SubscriptionImpl.java
@@ -1,5 +1,6 @@
 package org.example.sec01.publisher;
 
+import com.github.javafaker.Faker;
 import org.example.sec01.subscriber.SubscriberImpl;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
@@ -8,20 +9,40 @@ import org.slf4j.LoggerFactory;
 
 public class SubscriptionImpl implements Subscription {
     private static final Logger log = LoggerFactory.getLogger(Subscription.class);
+    public static final int MAX_ITEMS = 10;
+    private final Faker faker;
 
-    private Subscriber<? super String> subscriber;
+    private final Subscriber<? super String> subscriber;
+    private boolean isCancelled;
+    private int count = 0;
 
     public SubscriptionImpl(Subscriber<? super String> subscriber) {
         this.subscriber = subscriber;
+        this.faker = Faker.instance();
     }
 
     @Override
-    public void request(long l) {
+    public void request(long requested) {
+        if(isCancelled) {
+            return;
+        }
 
+        log.info("subscriber has requested {} items", requested);
+        for(int i = 0; i < requested && count < MAX_ITEMS; i++) {
+            count++;
+            this.subscriber.onNext(faker.internet().emailAddress());
+        }
+
+        if(count == MAX_ITEMS){
+            log.info("No more data to produce");
+            this.subscriber.onComplete();
+            this.isCancelled = true;
+        }
     }
 
     @Override
     public void cancel() {
-
+        log.info("Subscriber has cancelled");
+        isCancelled = true;
     }
 }
