13a225f617246aba106943d2066013f0c3671eec
Ayodeji Bankole <bankymono@gmail.com>
2025-08-03 21:21:26 +0100

    #

---
+++ src/main/java/org/example/common/AbstractHttpClient.java
@@ -0,0 +1,14 @@
+package org.example.common;
+
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.LoopResources;
+
+public class AbstractHttpClient {
+    public static final String BASE_URL = "http://localhost:7070";
+    protected final HttpClient httpClient;
+
+    public AbstractHttpClient() {
+        var loopResources = LoopResources.create("vins", 1, true);
+        this.httpClient = HttpClient.create().runOn(loopResources).baseUrl(BASE_URL);
+    }
+}
