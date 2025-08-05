3190a524cfc5ea528bccfcff20438e05a26f59bb
Ayodeji Bankole <bankymono@gmail.com>
2025-08-05 21:01:36 +0100

    #

---
+++ src/main/java/org/example/sec02/Lec11NonBlockingIO.java
@@ -0,0 +1,22 @@
+package org.example.sec02;
+
+import org.example.common.Util;
+import org.example.sec02.client.ExternalServiceClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Lec11NonBlockingIO {
+    private static final Logger log = LoggerFactory.getLogger(Lec11NonBlockingIO.class);
+
+    public static void main(String[] args) {
+        var client = new ExternalServiceClient();
+
+        log.info("starting");
+        for(int i = 1; i <= 50; i++) {
+            client.getProductName(i)
+                    .subscribe(Util.subscriber());
+        }
+
+        Util.sleepSeconds(2);
+    }
+}
