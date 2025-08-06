c5755d554843002691ebd7db340a3915f0b748f6
Ayodeji Bankole <bankymono@gmail.com>
2025-08-06 21:00:02 +0100

    #

---
+++ src/main/java/org/example/sec02/Lec12Assignment.java
@@ -0,0 +1,18 @@
+package org.example.sec02;
+
+import org.example.common.Util;
+import org.example.sec02.assignment.FileService;
+import org.example.sec02.assignment.FileServiceImpl;
+
+public class Lec12Assignment {
+    public static void main(String[] args) {
+        var fileService = new FileServiceImpl();
+        fileService.write("file.txt", "This is a test file")
+                        .subscribe(Util.subscriber());
+        fileService.read("file.txt")
+                .subscribe(Util.subscriber());
+
+        fileService.delete("file.txt")
+                .subscribe(Util.subscriber());
+    }
+}
