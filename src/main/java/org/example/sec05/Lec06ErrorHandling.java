bc1d2fb82534986fae046164c6479855eeb883e4
Ayodeji Bankole <bankymono@gmail.com>
2025-08-23 19:39:44 +0100

    #

--- src/main/java/org/example/sec05/Lec06ErrorHandling.java
+++ src/main/java/org/example/sec05/Lec06ErrorHandling.java
@@ -4,13 +4,19 @@ import org.example.common.Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class Lec06ErrorHandling {
     private static final Logger log = LoggerFactory.getLogger(Lec06ErrorHandling.class);
 
     public static void main(String[] args) {
-        Flux.range(1,10)
+//        Flux.range(1,10)
+//                .map(i -> i == 5 ? 5/0: i)
+//                .subscribe(Util.subscriber());
+
+        Flux.just(5)
                 .map(i -> i == 5 ? 5/0: i)
+                .onErrorResume(ex-> fallback())
                 .subscribe(Util.subscriber());
     }
 
@@ -21,5 +27,9 @@ public class Lec06ErrorHandling {
                 .onErrorReturn(ArithmeticException.class, -2)
                 .onErrorReturn(-3)
                 .subscribe(Util.subscriber());
+    }
+
+    private static Mono<Integer> fallback() {
+        return Mono.fromSupplier(() -> Util.faker().random().nextInt(10,100));
     }
 }
