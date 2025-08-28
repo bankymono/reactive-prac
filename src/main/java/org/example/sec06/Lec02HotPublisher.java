601c3ff79c665a994a2a294c2c7a87c424598dac
Ayodeji Bankole <bankymono@gmail.com>
2025-08-28 21:00:35 +0100

    #

---
+++ src/main/java/org/example/sec06/Lec02HotPublisher.java
@@ -0,0 +1,44 @@
+package org.example.sec06;
+
+import org.example.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class Lec02HotPublisher {
+    private static final Logger log = LoggerFactory.getLogger(Lec02HotPublisher.class);
+    public static void main(String[] args) {
+        var movieFlux = movieStream().share();
+
+        Util.sleepSeconds(2);
+        movieFlux
+                .take(4).subscribe(Util.subscriber("sam"));
+
+        Util.sleepSeconds(3);
+        movieFlux
+                .take(3)
+                .subscribe(Util.subscriber("mike"));
+
+        Util.sleepSeconds(15);
+
+    }
+
+    private static Flux<String> movieStream(){
+        return Flux.generate(
+                () -> {
+                    log.info("received the request");
+                    return 1;
+                },
+                (state, sink) -> {
+                    var scene = "movie scene" + state;
+                    log.info("playing {}", scene);
+                    sink.next(scene);
+                    return ++state;
+                }
+        ).take(10)
+                .delayElements(Duration.ofSeconds(1))
+                .cast(String.class);
+    }
+}
