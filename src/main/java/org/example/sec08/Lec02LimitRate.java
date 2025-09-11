c079e381cc2182b381a4166496798c7928d557af
Ayodeji Bankole <bankymono@gmail.com>
2025-09-11 21:02:31 +0100

    #

--- src/main/java/org/example/sec08/Lec02LimitRate.java
+++ src/main/java/org/example/sec08/Lec02LimitRate.java
@@ -1,4 +1,38 @@
 package org.example.sec08;
 
+import org.example.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
 public class Lec02LimitRate {
+    private static final Logger log = LoggerFactory.getLogger(Lec02LimitRate.class);
+
+    public static void main(String[] args) {
+
+        var producer = Flux.generate(
+                        () -> 1,
+                        (state, sink) -> {
+                            log.info("generating {}", state);
+                            sink.next(state);
+                            return ++state;
+                        }
+                ).cast(Integer.class)
+                .subscribeOn(Schedulers.parallel());
+
+        producer
+                .limitRate(5)
+                .publishOn(Schedulers.boundedElastic())
+                .map(i -> timeConsumingTask(i))
+                .subscribe(Util.subscriber());
+
+
+        Util.sleepSeconds(60);
+    }
+
+    private static int timeConsumingTask(int i) {
+        Util.sleepSeconds(1);
+        return i;
+    }
 }
