1c752932624f207d4a89d7dbb73f158f0dfd6991
Ayodeji Bankole <bankymono@gmail.com>
2025-09-18 21:06:50 +0100

    #

---
+++ src/main/java/org/example/sec08/Lec03MultipleSubscribers.java
@@ -0,0 +1,43 @@
+package org.example.sec08;
+
+import org.example.common.Util;
+import org.example.sec07.Lec03MultipleSubscribeOn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class Lec03MultipleSubscribers {
+    private static final Logger log = LoggerFactory.getLogger(Lec03MultipleSubscribeOn.class);
+
+    public static void main(String[] args) {
+
+        var producer = Flux.generate(
+                        () -> 1,
+                        (state, sink) -> {
+                            log.info("generating {}", state);
+                            sink.next(state);
+                            return ++state;
+                        }
+                ).cast(Integer.class)
+                .subscribeOn(Schedulers.parallel());
+
+        producer
+                .limitRate(5)
+                .publishOn(Schedulers.boundedElastic())
+                .map(i -> timeConsumingTask(i))
+                .subscribe(Util.subscriber("sub1"));
+
+        producer
+                .take(100)
+                .publishOn(Schedulers.boundedElastic())
+                .subscribe(Util.subscriber("sub2"));
+
+        Util.sleepSeconds(60);
+    }
+
+    private static int timeConsumingTask(int i) {
+        Util.sleepSeconds(1);
+        return i;
+    }
+}
