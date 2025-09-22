2ba1160739afd0d8aee8fa5ac55ab6a26a76c3f7
Ayodeji Bankole <bankymono@gmail.com>
2025-09-22 20:36:57 +0100

    #

---
+++ src/main/java/org/example/sec08/Lec05BackPressureStrategies.java
@@ -0,0 +1,45 @@
+package org.example.sec08;
+
+import org.example.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+
+public class Lec05BackPressureStrategies {
+    private static final Logger log = LoggerFactory.getLogger(Lec05BackPressureStrategies.class);
+
+    public static void main(String[] args) {
+        var producer = Flux.create(
+                        sink->{
+                            for(int i = 0; i <= 500 && !sink.isCancelled(); i++ ) {
+                                log.info("generating {}", i);
+                                sink.next(i);
+                                Util.sleep(Duration.ofMillis(50));
+                            }
+                            sink.complete();
+                        }
+                ).cast(Integer.class)
+                .subscribeOn(Schedulers.parallel());
+
+        producer
+//                .onBackpressureBuffer()
+//                .onBackpressureError()
+                .onBackpressureBuffer(10)
+                .limitRate(1)
+                .publishOn(Schedulers.boundedElastic())
+                .map(i -> timeConsumingTask(i))
+                .subscribe( );
+
+
+        Util.sleepSeconds(60);
+    }
+
+    private static int timeConsumingTask(int i) {
+        log.info("received: {}", i);
+        Util.sleepSeconds(1);
+        return i;
+    }
+}
